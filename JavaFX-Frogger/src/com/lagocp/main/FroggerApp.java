9fd1f9aa85ff7622c6ae6ae6dec346e2d38ea3e9
philliplagoc <philliplagoc@gmail.com>
2017-09-01 10:29:29 -0700

    Created main application class

---
+++ JavaFX-Frogger/src/com/lagocp/main/FroggerApp.java
@@ -0,0 +1,18 @@
+package com.lagocp.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FroggerApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
